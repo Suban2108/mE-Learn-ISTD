1d42787ddf81af6d9a0072845dff66c5635d12e1
Abdul Suban <122554707+Suban2108@users.noreply.github.com>
2025-01-11 12:25:43 +0530

    Updated the Cors

--- Backend/index.js
+++ Backend/index.js
@@ -6,13 +6,12 @@ const cors = require("cors");
 
 const app = express();
 
-const corsOptions = {
-    origin: "http://localhost:3000",
-    methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-    credentials: true,
-};
+app.use(cors({
+  origin: 'https://m-e-learn-istd.vercel.app', // Frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
